a3d2585d712950d0ae26c834d8b375567d55538f
AnaBeCosta <anacost.a@outlook.com>
2025-05-29 20:58:09 -0300

    First commit

---
+++ src/main/java/com/example/mepaga/MepagaApplication.java
@@ -0,0 +1,13 @@
+package com.example.mepaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MepagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MepagaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mepaga/MepagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mepaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MepagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
