be4320386635cf19c0bbd42b5100931ddb81cdcb
AnaBeCosta <anacost.a@outlook.com>
2025-06-08 23:56:07 -0300

    Criação de entidades e ajustes de segurança

---
+++ src/main/java/com/anabeatriz/mepaga/model/Usuario.java
@@ -0,0 +1,29 @@
+package com.anabeatriz.mepaga.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String name;
+
+    @ManyToOne
+    private OrigemContato origem_contato;
+}
